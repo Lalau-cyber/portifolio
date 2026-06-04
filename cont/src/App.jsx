57a007d8ccb3dacad2e8768694f3950ee60c2cd2
Laura <laurinha.ss1305@gmail.com>
2026-06-04 17:28:52 -0300

    Update App.jsx

--- cont/src/App.jsx
+++ cont/src/App.jsx
@@ -1,12 +1,10 @@
 
-import ContadorLimitado from './components/contadorLimitado';
 import ListaTarefas from './components/ListaTarefas';
 import './App.css'
 
 export default function App() {
   return(
  <div>
-  <ContadorLimitado/>
   <ListaTarefas/>
  </div>
   )
